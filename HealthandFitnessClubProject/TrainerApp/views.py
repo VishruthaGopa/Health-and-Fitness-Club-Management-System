c2a7b015ebad8048a846b4c905418a935ba15226
VishruthaGopa <vishrutha.gopa@gmail.com>
2024-03-30 14:19:15 -0400

    search bar functionality

--- HealthandFitnessClubProject/TrainerApp/views.py
+++ HealthandFitnessClubProject/TrainerApp/views.py
@@ -5,16 +5,20 @@ import psycopg2
 # Create your views here.
 def members(request):
     print("member view")
-    users = displayMembers()
+    search_query = request.GET.get('search', '')  # Get the search query from the request
+    users = displayMembers(search_query)  # Pass the search query to displayMembers
     print(users)  # testing
-    
     return render(request, 'TrainerApp/members.html', {'users': users})
 
-# Display the Members table
-def displayMembers():
+# Display the Members table with optional search query
+def displayMembers(search_query=None):
     connection = connect()
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM \"User\" where role = 'member'")
+    if search_query:  # Check if search_query is not empty
+        # Use LIKE operator to search for names containing the search query
+        cursor.execute("SELECT * FROM \"User\" WHERE role = 'member' AND username ILIKE %s", ('%' + search_query + '%',))
+    else:
+        cursor.execute("SELECT * FROM \"User\" WHERE role = 'member'")
     users = cursor.fetchall()
     connection.close()
     return users
