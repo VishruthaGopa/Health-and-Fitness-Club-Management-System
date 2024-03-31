96b8caa668cc500713c8c526a46fd7d9505542e8
ermeen-el <nermeenelsherbiny8@gmail.com>
2024-03-31 16:20:36 -0400

    changed route

--- HealthandFitnessClubProject/HealthandFitnessClubApp/views.py
+++ HealthandFitnessClubProject/HealthandFitnessClubApp/views.py
@@ -28,7 +28,7 @@ def home(request):
                 elif user_info['role'] == 'trainer':
                     return redirect('TrainerApp-trainer_profile', user_id=user_info['user_id'])
                 elif user_info['role'] == 'admin':
-                    return redirect('AdminApp-display_members') 
+                    return redirect('AdminApp-home') 
 
             else:
                 print("Incorrect username or password.")
