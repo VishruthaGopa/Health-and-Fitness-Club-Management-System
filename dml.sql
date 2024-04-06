d78dd0ae92399c3ddc44b8a2442179ece4e6a6e1
VishruthaGopa <vishrutha.gopa@gmail.com>
2024-04-06 16:06:28 -0400

    update dml

--- dml.sql
+++ dml.sql
@@ -11,7 +11,7 @@ INSERT INTO "User" (username, password, role) VALUES
 -- Member table
 INSERT INTO Member (member_id, first_name, last_name, gender, email, date_of_birth, address, phone_number, start_date, payment_status) VALUES
 (1, 'John', 'Doe', 'Male', 'john@example.com', '1990-01-01', '123 Main St, City', '1234567890', '2024-01-01', true),
-(2, 'Jane', 'Doe', 'Female', 'jane@example.com', '1995-05-05', '456 Elm St, Town', '0987654321', '2024-02-15', true);
+(2, 'Jane', 'Smith', 'Female', 'jane@example.com', '1995-05-05', '456 Elm St, Town', '0987654321', '2024-02-15', true);
 
 -- Member_Health table
 INSERT INTO Member_Health (member_id, start_weight, current_weight, height, age) VALUES
@@ -96,15 +96,15 @@ INSERT INTO Personal_Training_Sessions (trainer_id, member_id, session_date, ses
 (5, 2, CURRENT_DATE + INTERVAL '1' DAY, '18:00:00', 9, 35.00, true),
 (5, 1, CURRENT_DATE + INTERVAL '1' DAY, '20:00:00', 14, 50.00, true),
 (4, NULL, CURRENT_DATE + INTERVAL '2' DAY, '17:00:00', 23, 45.00, NULL),
-(5, 2, CURRENT_DATE + INTERVAL '2' DAY, '19:00:00', 28, 45.00, true),
+(4, 2, CURRENT_DATE + INTERVAL '2' DAY, '19:00:00', 28, 45.00, true),
 (5, 1, CURRENT_DATE + INTERVAL '2' DAY, '21:00:00', 36, 40.00, true),
 (4, NULL, CURRENT_DATE + INTERVAL '3' DAY, '18:00:00', 45, 40.00, NULL),
 (5, 1, CURRENT_DATE + INTERVAL '3' DAY, '20:00:00', 51, 50.00, true),
-(4, NULL, CURRENT_DATE + INTERVAL '3' DAY, '21:00:00', 52, 40.00, NULL);
+(5, NULL, CURRENT_DATE + INTERVAL '3' DAY, '21:00:00', 52, 40.00, NULL);
 
 -- Group_Fitness_Classes table
 INSERT INTO Group_Fitness_Classes (trainer_id, room_id, class_name, description, session_date, session_time, member_ids) VALUES
-(4, 4, 'Evening Pilates', 'Pilates class for all levels', CURRENT_DATE + INTERVAL '1' DAY, '17:00:00', '{1, 2}'),
+(4, 4, 'Evening Pilates', 'Pilates class for all levels', CURRENT_DATE + INTERVAL '1' DAY, '17:00:00', '{}'),
 (5, 18, 'Strength Training', 'Focus on building strength and muscle', CURRENT_DATE + INTERVAL '1' DAY, '21:00:00', '{1, 2}'),
 (5, 25, 'Cardio Blast', 'High-intensity cardio workout', CURRENT_DATE + INTERVAL '2' DAY, '18:00:00', '{2}'),
 (5, 29, 'Cardio', 'Cardio workout', CURRENT_DATE + INTERVAL '2' DAY, '19:00:00', '{}'),
@@ -126,7 +126,7 @@ FROM Group_Fitness_Classes AS gfc
 WHERE rb.room_id = gfc.room_id
   AND rb.time = gfc.session_time
   AND rb.date = gfc.session_date;
-  
+
 -- Equipment_Maintenance table
 INSERT INTO Equipment_Maintenance (equipment_name, last_maintained_date, next_maintenance, performed_by) VALUES
 ('Treadmill', '2024-01-01', '2024-04-01', 3),
